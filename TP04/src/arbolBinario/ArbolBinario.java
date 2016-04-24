47d49cbee0534172ec80155a7186f548b3ca6fbd
fede054 <fpaulettich@gmail.com>
2016-04-24 15:33:38 -0300

    Hice el lleno y frontera

--- TP04/src/arbolBinario/ArbolBinario.java
+++ TP04/src/arbolBinario/ArbolBinario.java
@@ -1,6 +1,8 @@
 package arbolBinario;
 
 import tp03.ejercicio5.Lista;
+import tp03.ejercicio5.ListaEnlazada;
+import tp03.ejercicio7.ColaGenerica;
 import nodoBinario.NodoBinario;
 
 public class ArbolBinario {
@@ -65,13 +67,13 @@ public class ArbolBinario {
 	*/
 	
 	public boolean esVacio() {
-		// Falta implementar. 
-		return true;
+		// Falta implementar.
+		return (this.raiz == null);
 	}
 
 	public boolean esHoja() {
-		// Falta implementar. 
-		return true;
+		// Falta implementar.
+		return (this.getHijoDerecho().getDatoRaiz() == null && this.getHijoIzquierdo().getDatoRaiz() == null);
 	}
 	
 	public String toString() {
@@ -80,15 +82,71 @@ public class ArbolBinario {
 	}
 	
 	
+	private Lista hojas(Lista l) {
+		if (!this.esVacio()) {
+			if (this.esHoja())
+				l.agregarFinal(this.getDatoRaiz());
+			else {
+				this.getHijoIzquierdo().hojas(l);
+				this.getHijoDerecho().hojas(l);
+			}
+		}
+		return l;
+	}
+	
 	public Lista frontera() {
 		// Falta implementar. Ejercicio 2.a de la práctica
-		return null;
+		Lista l = new ListaEnlazada();
+		return this.hojas(l);
 	}
+	
+	private boolean full (int[] tmp){
+		int[] vizq = {0};
+		int[] vder = {0};
+		boolean ok = false;
+		if (this.esHoja())
+			tmp[0]++;
+		else {
+			if (!this.getHijoIzquierdo().esVacio()){
+				if (this.getHijoIzquierdo().full(vizq))
+					vizq[0]++;
+			}
+			if (!this.getHijoDerecho().esVacio()) {
+				if (this.getHijoDerecho().full(vder))
+					vder[0]++;
+			}
+		}
+		ok = vizq[0] == vder[0];
+		return ok;
+	}
+	
+	/*
+	
+	private boolean full2 (int[] tmp){
+		ColaGenerica<Integer> cola = new ColaGenerica<Integer>();
+		//int[] vder = {0};
+		boolean ok = false;
+		if (this.esHoja())
+			tmp[0]++;
+		else
+			if (!this.getHijoIzquierdo().esVacio()){
+				vizq[0]++;
+				this.getHijoIzquierdo().full2(vizq);
+			}
+			if (!this.getHijoDerecho().esVacio()){
+				vder[0]++;
+				this.getHijoDerecho().full2(vder);
+			}
+		ok = vizq[0] == vder[0];
+		return ok;
+	}
+		
+	*/ 
 	
 	public boolean lleno() {
 		//Falta implementar. Ejercicio 2.b de la práctica
-		return true;
-
+		int[] tmp = {0};
+		return this.full(tmp);
 	}
 	
 	public boolean completo() {
